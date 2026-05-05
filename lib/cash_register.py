751416a5d1b7311563a84d9fe2ab3ff7767ca505
Alyssa Mercado <amercado1199@gmail.com>
2026-05-05 11:49:58 -0400

    Completed OOP-P2-lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,50 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, discount):
+        if isinstance(discount, int) and 0 <= discount <= 100:
+            self._discount = discount
+        else:
+            print("Not valid discount")
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+
+        for i in range(quantity):
+            self.items.append(item)
+
+        transaction = {
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        }
+
+        self.previous_transactions.append(transaction)
+
+    def apply_discount(self):
+        if self.total == 0:
+            print("There is no discount to apply.")
+        else:
+            self.total -= self.total * (self.discount / 100)
+            print(f"After the discount, the total comes to ${int(self.total)}.")
+
+    def void_last_transaction(self):
+        if len(self.previous_transactions) == 0:
+            print("There is no transaction to void.")
+        else:
+            last_transaction = self.previous_transactions.pop()
+            self.total -= last_transaction["price"] * last_transaction["quantity"]
+
+            if last_transaction["item"] in self.items:
+                self.items.remove(last_transaction["item"])
